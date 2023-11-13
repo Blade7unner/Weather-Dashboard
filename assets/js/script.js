e5c8e809ecbcb86e61cf0ab1c7c856e7ce3f8777
Benoit <fbenoit2077@gmail.com>
2023-11-13 15:03:54 -0500

    function getcurrentweather edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // Example of fetching current weather data
 function getCurrentWeather(city) {
     // Use fetch to get current weather data from the OpenWeatherMap API
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={634b0cc73b3b80106e8b3e30353b60fb}`)
-  .then(response => response.json())
-  .then(data => {
-    // Extract relevant information from data and update UI
-    updateCurrentWeatherUI(data);
-  })
-  .catch(error => {
-    console.error('Error fetching current weather:', error);
-  });
-}
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={YOUR_API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        // Extract relevant information from data and update UI
+        updateCurrentWeatherUI(data);
+      })
+      .catch(error => {
+        console.error('Error fetching current weather:', error);
+      });
+  }
 
 // Example of updating the UI with current weather data
 function updateCurrentWeatherUI(data) {
