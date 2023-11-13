c920fc976bad86f8406bcb454a65a3fa04def2ed
Benoit <fbenoit2077@gmail.com>
2023-11-13 15:12:14 -0500

    deleted old js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,37 +58,3 @@ $("#city-form").submit(function (event) {
     getWeatherForecast(city);
   });
 
-  
-
-
-
-// Example of updating the UI with current weather data
-function updateCurrentWeatherUI(data) {
-    // Update the UI to display current weather information
-    // Access data properties such as data.name, data.main.temp, etc.
-  }
-
-  // Example of fetching forecast data
-function getWeatherForecast(city) {
-    // Use fetch to get 5-day forecast data from the OpenWeatherMap API
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid={634b0cc73b3b80106e8b3e30353b60fb}`)
-      .then(response => response.json())
-      .then(data => {
-        // Extract relevant information from data and update UI
-        updateForecastUI(data);
-      })
-      .catch(error => {
-        console.error('Error fetching weather forecast:', error);
-      });
-  }
-
-  // Example of updating the UI with forecast data
-function updateForecastUI(data) {
-    // Update the UI to display 5-day forecast information
-    // Access data properties such as data.list[0].dt_txt, data.list[0].main.temp, etc.
-  }
-
-
-
-
-  
