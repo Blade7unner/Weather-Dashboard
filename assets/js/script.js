f08123852c38a14c3ab0de7c0c7a6a61d9bd4c35
Benoit <fbenoit2077@gmail.com>
2023-11-13 15:10:45 -0500

    updateforecastUI function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,33 @@ function updateCurrentWeatherUI(data) {
     // For example, $("#current-weather").text(`City: ${cityName}, Temperature: ${temperature}`);
   }
 
+  // Example of fetching forecast data
+function getWeatherForecast(city) {
+    // Use fetch to get 5-day forecast data from the OpenWeatherMap API
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid={YOUR_API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        // Extract relevant information from data and update UI
+        updateForecastUI(data);
+      })
+      .catch(error => {
+        console.error('Error fetching weather forecast:', error);
+      });
+  }
+
+// Example of updating the UI with forecast data
+function updateForecastUI(data) {
+    // Update the UI to display 5-day forecast information
+    // Access data properties such as data.list[0].dt_txt, data.list[0].main.temp, etc.
+    // Loop through the forecast data and update the UI accordingly
+    for (const forecast of data.list) {
+      const date = forecast.dt_txt;
+      const temperature = forecast.main.temp;
+      // Update the UI elements with the retrieved forecast data
+      // For example, $("#forecast").append(`Date: ${date}, Temperature: ${temperature}<br>`);
+    }
+  }
+
   
 
 
