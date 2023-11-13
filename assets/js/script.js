7e920778d86fa2b7a07ad3d88bf9f1e5980a399b
Benoit <fbenoit2077@gmail.com>
2023-11-13 13:39:36 -0500

    openweather api link added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 // Example of fetching current weather data
 function getCurrentWeather(city) {
+    // Use fetch to get current weather data from the OpenWeatherMap API
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={YOUR_API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    // Extract relevant information from data and update UI
+    updateCurrentWeatherUI(data);
+  })
+  
