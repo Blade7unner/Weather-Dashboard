d03165d3d49bbd61868a0f479680a3d49be08a23
Benoit <fbenoit2077@gmail.com>
2023-11-13 13:41:09 -0500

    function to add the updated weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,15 @@ function getCurrentWeather(city) {
     // Extract relevant information from data and update UI
     updateCurrentWeatherUI(data);
   })
+  .catch(error => {
+    console.error('Error fetching current weather:', error);
+  });
+}
+
+// Example of updating the UI with current weather data
+function updateCurrentWeatherUI(data) {
+    // Update the UI to display current weather information
+    // Access data properties such as data.name, data.main.temp, etc.
+  }
+  
   
